c42f71d43b851fa088478de6b185465526aad30a
Nour Eldin Megahed <77926574+noureldinmegahed@users.noreply.github.com>
2021-05-10 14:41:13 +0200

    Update script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,49 +1,44 @@
 var canvas1 = new fabric.Canvas('c1');
 var square = new fabric.Rect({
-  left: 200,
-  top: 0,
-  fill: 'red',
-  width: 40,
-  height: 40
+  left: 125, top: 50, fill: 'red', width: 40, height: 40
 });
-var mostatel = new fabric.Rect({
-  left: 200,
-  top: 50,
-  fill: 'purple',
-  width: 80,
-  height: 40
+var rectangle = new fabric.Rect({
+  left: 120, top: 50, fill: 'purple', width: 80, height: 40
 });
-var Rectangle = new fabric.Rect({
-  left: 200,
-  top: 150,
-  fill: 'grey',
-  width: 160,
-  height: 20
-});
-
-// "add" rectangle onto canvas
-canvas1.add(square);
-var canvas = new fabric.Canvas('drawer');
-// "add" rectangle onto canvas
 var circle = new fabric.Circle({
-  radius: 20, fill: 'green', left: 100, top: 0
+  radius: 20, fill: 'green', left: 130, top: 50
 });
 var triangle = new fabric.Triangle({
-  width: 40, height: 40, fill: 'blue', left: 0, top: 0
+  width: 40, height: 40, fill: 'blue', left: 120, top: 50
 });
-canvas.add(circle, triangle, square, mostatel , Rectangle);
-var heightOfRectangle,widthOfRectangle;
+// "add" rectangle onto canvas
+var rectangleCanvas = new fabric.Canvas('rectangleDrawer');
+var squareCanvas = new fabric.Canvas('squareDrawer');
+var triangleCanvas = new fabric.Canvas('triangleDrawer');
+var circleCanvas = new fabric.Canvas('circleDrawer');
+rectangleCanvas.add(rectangle);
+squareCanvas.add(square)
+triangleCanvas.add(triangle)
+circleCanvas.add(circle)
 // setInterval(function () {
 //   document.getElementById("lengthOfTheSide").innerText = parseFloat((square.width * square.scaleX) * 0.0264583333).toFixed(2);
 //   document.getElementById("X").innerText = square.top;
 //   document.getElementById("Y").innerText = square.left;
 // }, 1);
+// "add" rectangle onto canvas
+
+
+
+
+
+
+var heightOfRectangle,widthOfRectangle;
 setInterval(function () {
-  heightOfRectangle = document.getElementById("height2").innerText = parseFloat((mostatel.height * mostatel.scaleY) * 0.0264583333).toFixed(2);
-  widthOfRectangle = document.getElementById("width2").innerText = parseFloat((mostatel.width * mostatel.scaleX) * 0.0264583333).toFixed(2);
-  document.getElementById("Msa7aOfMostatel").innerText = parseFloat(widthOfRectangle * heightOfRectangle).toFixed(2)
-  document.getElementById("X2").innerText = parseFloat(mostatel.top).toFixed(2);
-  document.getElementById("Y2").innerText = parseFloat(mostatel.left).toFixed(2);
+  heightOfRectangle = document.getElementById("height2").innerText = parseFloat((rectangle.height * rectangle.scaleY) * 0.0264583333).toFixed(2);
+  widthOfRectangle = document.getElementById("width2").innerText = parseFloat((rectangle.width * rectangle.scaleX) * 0.0264583333).toFixed(2);
+  document.getElementById("rectangleArea").innerText = parseFloat(widthOfRectangle * heightOfRectangle).toFixed(2)
+  document.getElementById("XNum").innerText = parseFloat(rectangle.top).toFixed(2);
+  document.getElementById("YNum").innerText = parseFloat(rectangle.left).toFixed(2);
 }, 300);
 function submitAnwser(){
     if(widthOfRectangle == "4.24" && heightOfRectangle == "2.12"){

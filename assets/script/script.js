b5985ad60587e4b2bc1ecfcf0dcaf6f2365d2dad
Nour Eldin Megahed <77926574+noureldinmegahed@users.noreply.github.com>
2021-05-11 14:18:43 +0200

    Update script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,7 +6,7 @@ var rectangle = new fabric.Rect({
   left: 120, top: 50, fill: 'purple', width: 80, height: 40
 });
 var circle = new fabric.Circle({
-  radius: 20, fill: 'green', left: 130, top: 50
+  radius: 20, fill: 'green', left: 130, top: 50, width: 70
 });
 var triangle = new fabric.Triangle({
   width: 40, height: 40, fill: 'blue', left: 120, top: 50
@@ -20,18 +20,22 @@ rectangleCanvas.add(rectangle);
 squareCanvas.add(square)
 triangleCanvas.add(triangle)
 circleCanvas.add(circle)
-// setInterval(function () {
-//   document.getElementById("lengthOfTheSide").innerText = parseFloat((square.width * square.scaleX) * 0.0264583333).toFixed(2);
-//   document.getElementById("X").innerText = square.top;
-//   document.getElementById("Y").innerText = square.left;
-// }, 1);
-// "add" rectangle onto canvas
 
-
-
-
-
-
+setInterval(function () {
+  document.getElementById("lengthOfTheSide").innerText = parseFloat((square.width * square.scaleX) * 0.0264583333).toFixed(2);
+  document.getElementById("X").innerText = parseFloat(square.top).toFixed(2);
+  document.getElementById("Y").innerText = parseFloat(square.left).toFixed(2);
+}, 200);
+setInterval(function () {
+  document.getElementById("Diameter").innerText = parseFloat((circle.width * circle.scaleX) * 0.0264583333).toFixed(2);
+  document.getElementById("X3").innerText = parseFloat(circle.top).toFixed(2);
+  document.getElementById("Y3").innerText = parseFloat(circle.left).toFixed(2);
+}, 200);
+setInterval(function () {
+  document.getElementById("length").innerText = parseFloat((triangle.width * triangle.scaleX) * 0.0264583333).toFixed(2);
+  document.getElementById("X4").innerText = parseFloat(triangle.top).toFixed(2);
+  document.getElementById("Y4").innerText = parseFloat(triangle.left).toFixed(2);
+}, 200);
 var heightOfRectangle,widthOfRectangle;
 setInterval(function () {
   heightOfRectangle = document.getElementById("height2").innerText = parseFloat((rectangle.height * rectangle.scaleY) * 0.0264583333).toFixed(2);
@@ -39,7 +43,7 @@ setInterval(function () {
   document.getElementById("rectangleArea").innerText = parseFloat(widthOfRectangle * heightOfRectangle).toFixed(2)
   document.getElementById("XNum").innerText = parseFloat(rectangle.top).toFixed(2);
   document.getElementById("YNum").innerText = parseFloat(rectangle.left).toFixed(2);
-}, 300);
+}, 200);
 function submitAnwser(){
     if(widthOfRectangle == "4.24" && heightOfRectangle == "2.12"){
     alert("success")
